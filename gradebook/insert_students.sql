7207a415e36bd1c2c4c615f740a3c779a4951548
Gary Rozanc <grozanc@gmail.com>
2020-01-25 21:58:57 -0500

    Removed old data

--- gradebook/insert_students.sql
+++ gradebook/insert_students.sql
@@ -2,35 +2,8 @@ http://www.sha1-online.com/
 
 INSERT INTO students (name, id_email, id_campus, id_course) VALUES 
 					 ('name', 'campus_email', 'campus_id', 'course'),
+					 ('name', 'campus_email', 'campus_id', 'course'),
+					 ('name', 'campus_email', 'campus_id', 'course'),
+					 ('name', 'campus_email', 'campus_id', 'course'),
 					 ('name', 'campus_email', 'campus_id', 'course');
-					 
-INSERT INTO students (name, id_email, id_campus, id_course) VALUES 
-					 ('Bunmi A.', 'oadenij2@umbc.edu', '2bdf4f42d41f5bd76331b75fe6b4b193a9982a7a', '2020s_art336_01'),
-					 ('Matthew B.', 'matt47@umbc.edu', '8d9d95c4e14abf07573d1f0a21fa3987b6c1194c', '2020s_art336_01'),
-					 ('Michael E.', 'meckard1@umbc.edu', 'de5db71fad5e1b68fd4d6d5224cbc4bbf7a93e8d', '2020s_art336_01'),
-					 ('Kyah E.', 'kedward4@umbc.edu', 'dc834d5088cf7704f3387373d3f39fcb4bb58c26', '2020s_art336_01'),
-					 ('Kimberley G.', 'kimberg1@umbc.edu', '305a038989bf894891e3ee680800579b2ddbf68a', '2020s_art336_01'),
-					 ('Daniel K.', 'dkim29@umbc.edu', '379d5330a674e00cc05418087698e23e5d486035', '2020s_art336_01'),
-					 ('Gretchen M.', 'gmarcus1@umbc.edu', '9f80b6bb22e38df3c208b3a539c66fbaa0538989', '2020s_art336_01'),
-					 ('Aristides M.', 'amilan2@umbc.edu', '47afce62be79239640515c75f5c28eb660e185f3', '2020s_art336_01'),
-					 ('Caddain M.', 'm170@umbc.edu', 'dc06cdbb1397166b426c02b7273dd1d1262dc089', '2020s_art336_01'),
-					 ('Nyla R.', 'ZQ55535@umbc.edu', '6891b2172a54f8883193b9bf7332b6d8417c25a6', '2020s_art336_01'),
-					 ('Jennifer  S', 'jsatz1@umbc.edu', 'bce9ddaa80ae60d069ec0c70930e7b6bbe57f960', '2020s_art336_01'),
-					 ('Maddy S.', 'madshea1@umbc.edu', '68acec113399307940cf131f9bf1bf6574816096', '2020s_art336_01'),
-					 ('Jessie W.', 'jessiew1@umbc.edu', '8617480bcb8d3debee2d7e8b3b30f06c8dff0a16', '2020s_art336_01'),
-					 ('Yi D.', 'duyibe1@umbc.edu', '548e22e82236acb6d95ae5734eab3930cf5db178', '2020s_art336_01'),
-					 ('Annie F.', 'afuqua1@umbc.edu', 'f932332e0434b493c0cf9413950b3367aae0afb4', '2020s_art336_02'),
-					 ('Victoria G.', 'vgarcia1@umbc.edu', 'd942778f7c80a3daa1a6594e749ffa11184a6bf8', '2020s_art336_02'),
-					 ('Liam G.', 'lgarret1@umbc.edu', '1e1776279f3c96c5bf813a2151728667f1fc26c6', '2020s_art336_02'),
-					 ('Raquel H', 'rhamner1@umbc.edu', 'ced5043e487bbb61d25752c1d6d30783b362ece9', '2020s_art336_02'),
-					 ('Hannah H.', 'hhong3@umbc.edu', '813a6dcf72d9f1f43c6e5d6c7613dc3667b8cce6', '2020s_art336_02'),
-					 ('Elizabeth K.', 'elk3@umbc.edu', '1417193f80035d44c20b094c3c8519895aedf49d', '2020s_art336_02'),
-					 ('Ashlee K.', 'ashkrei1@umbc.edu', '31ba1601fc08649b6a2d0e635a6a78c2f5ef80d9', '2020s_art336_02'),
-					 ('Sean M.', 'mansean1@umbc.edu', '4737c64176ba3008e32c5e45a8d38a152695477a', '2020s_art336_02'),
-					 ('Shania M.', 'shania1@umbc.edu', '6d969f4325e93e0a77b492b06be415be45eff18c', '2020s_art336_02'),
-					 ('Ryan S.', 'rshadle1@umbc.edu', '6fbb32910396be329f3cdc3751fc12ea0429312b', '2020s_art336_02'),
-					 ('Jenny S.', 'jsmith45@umbc.edu', 'fac680c51a1384f30bd5d4d8077e4003f95fb982', '2020s_art336_02'),
-					 ('Montse S.', 'soto1@umbc.edu', 'b8fd6c16566287bf46dfac45e79bfa6692dba758', '2020s_art336_02'),
-					 ('Ami S.', 'amis2@umbc.edu', 'fbeef26a13c8bb00b6498c2bb425987ed95859f0', '2020s_art336_02'),
-					 ('Ethan W.', 'ethanw3@umbc.edu', 'f6ec4dda5551e84b438e1928e8aaef94a3584cf1', '2020s_art336_02');
 				
